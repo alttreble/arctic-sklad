eac8b42636bae5d3feb4bcf533e504df887b6da2
Axxxll <alex.plam.atan@gmail.com>
2022-08-12 19:58:16 +0300

    Updated package.json
    Няма никва стойност Брат!
    Само ще се наема

--- apps/frontend/src/routes/index.svelte
+++ apps/frontend/src/routes/index.svelte
@@ -1,6 +1,34 @@
 <script>
-    let autor = "Axl"
-    
+	let autor = 'Axl';
+	async function getUoms() {
+		let results = await fetch('http://localhost:4000/', {
+			method: 'POST',
+
+			headers: {
+				'Content-Type': 'application/json'
+			},
+
+			body: JSON.stringify({
+				query: `uoms {
+                            edges {
+                                node {
+                                    id
+                                    name
+                                    items {
+                                        name
+                                    }
+                                }
+                            }
+                        }`
+			})
+		});
+		let uoms = await results.json();
+        return uoms
+	}
+
+	let uoms = getUoms();
 </script>
 
 <h1>The autor is {autor}</h1>
+
+<p>{uoms.uoms.edges[0].node.name}</p>
