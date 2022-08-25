946290b226d099124748fa0438cca0c0eb0db446
Axxxll <alex.plam.atan@gmail.com>
2022-08-25 12:44:43 +0300

    Created an itemComonent.svelte.

--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,21 +1,18 @@
 <script lang="ts">
-  import { Card, CardContent, Container, Stack, Typography} from "../lib/index.js";
-  import type { ItemEdge } from "../generated/graphql";
-  let expert = "Axl"
+	import { Card, CardContent, Container, Stack, Typography } from '../lib/index.js';
+	import type { ItemEdge } from '../generated/graphql';
+  import ItemComonent from '../components/itemComonent.svelte';
+	
 
-  export let data: {items: ItemEdge[]}
-  const items = data?.items
+	export let data: { items: ItemEdge[] };
+	const items = data?.items;
 </script>
 
-<Container element="main">
-   <Stack gap={4}>
-      <Typography variant="subtitle1">The expert is <strong>{expert}</strong></Typography>
-    {#each items as item}
-        <Card>
-          <CardContent>
-            {item.node.uom.name}
-          </CardContent>
-        </Card>
-    {/each}
-  </Stack>
-</Container>
+<Stack gap={3}>
+{#each items as item}
+  
+    <ItemComonent item={item}/>
+  
+{/each}
+</Stack>
+
