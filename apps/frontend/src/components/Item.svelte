854a4d8c1e83ed278482afe2b4c0d64d12025f5f
Axxxll <alex.plam.atan@gmail.com>
2022-10-31 18:48:52 +0200

    Item cards now show all notification warnings

--- apps/frontend/src/components/Item.svelte
+++ apps/frontend/src/components/Item.svelte
@@ -1,7 +1,7 @@
-<script lang='ts'>
+<script lang="ts">
 	import { slide } from 'svelte/transition';
 	import { Icon } from '@steeze-ui/svelte-icon';
-	import { ChevronDown, ChevronUp, ExclamationCircle } from '@steeze-ui/heroicons';
+	import { ChevronDown, ChevronUp, ExclamationCircle, Exclamation, CheckCircle } from '@steeze-ui/heroicons';
 	import Divider from '$lib/components/Divider.svelte';
 	import type { ItemEdge, UomsDocument } from '../generated/graphql';
 	import { Button, Card, CardContent, Stack, Typography } from '../lib/index.js';
@@ -13,71 +13,82 @@
 	let itemEntriesBtn = false;
 
 	function toggleItemEntries(e) {
-		e.preventDefault()
+		e.preventDefault();
 		itemEntriesBtn = !itemEntriesBtn;
 	}
-
 </script>
 
-<Card class='bg-white' element='a' href={`item/${item.node?.id}`}>
-	<CardContent class='flex flex-col'>
-		<div class='flex'>
-			<Stack class='xs:flex-row sm:flex-col flex-1 justify-between'>
-				<Stack class='justify-between'>
-					<div class='pb-6'>
-						<Typography variant='h6'>
+<Card class="bg-white" element="a" href={`item/${item.node?.id}`}>
+	<CardContent class="flex flex-col">
+		<div class="flex">
+			<Stack class="xs:flex-row sm:flex-col flex-1 justify-between">
+				<Stack class="justify-between">
+					<div class="pb-6">
+						<Typography variant="h6">
 							{item.node?.name}
 						</Typography>
 						{#if item.node?.genericName}
-							<Typography class='text-sm' variant='subtitle1'>
+							<Typography class="text-sm" variant="subtitle1">
 								{item.node?.genericName}
 							</Typography>
 						{/if}
 					</div>
 				</Stack>
-				<Stack direction='row'>
+				<Stack direction="row">
 					<Typography>
-						<strong> {item.node?.totalQuantity} 
+						<strong>
+							{item.node?.totalQuantity}
 							{#if item.node?.totalQuantity === 1}
 								{item.node?.uom.name}
 							{:else}
 								{item.node?.uom.namePlural}
-							{/if}	
+							{/if}
 						</strong>
 					</Typography>
 				</Stack>
 			</Stack>
-			<div class='flex flex-col justify-center'>
-				<Button variant='text' on:click={toggleItemEntries}>
-					<Icon
-						class='w-5 h-5'
-						src={itemEntriesBtn ? ChevronUp : ChevronDown}
-					/>
+			<div class="flex flex-col justify-center">
+				<Button variant="text" on:click={toggleItemEntries}>
+					<Icon class="w-5 h-5" src={itemEntriesBtn ? ChevronUp : ChevronDown} />
 				</Button>
 			</div>
 		</div>
 		{#if itemEntriesBtn}
-			<div transition:slide={{duration: 200}} class='w-full'>
+			<div transition:slide={{ duration: 200 }} class="w-full">
 				<ItemEntries itemEntries={item?.node?.entries} />
 			</div>
 		{/if}
-		<!-- {#if item.node?.hasExpiredEntry}
-			<Divider class='my-4' />
-			<Typography class='flex'>
-				<Icon class='w-6 h-6 stroke-red-500 mr-3' src={ExclamationCircle} />
-				Изтекъл срок
+			<Divider class="my-4" />
+			{#if !item.node?.notifications?.find((e) => e?.type)}
+			<Typography class="flex">
+				<Icon class="w-[24px] h-[24px] stroke-green-500 mr-2" src={CheckCircle} />
+				Топ
 			</Typography>
-		{/if}
-		{#if item.node?.notifications?.find((e) => e?.type === "lowQuantity")}
-			<Divider class='my-4' />
-			<Typography class='flex'>
-				<Icon class='w-6 h-6 stroke-red-500 mr-3' src={ExclamationCircle} />
-				Ниско количество
-			</Typography>
-		{/if} -->
-		{#if item.node?.notifications?.find(e => e?.type)}
-			<Divider class='my-4' />
-		{/if}
+			{/if}
+			<div class="flex gap-5 flex-wrap">
+				{#if item.node?.notifications?.find((e) => e?.type === 'hasExpired')}
+				<Typography class="flex">
+					<Icon class="w-[24px] h-[24px] stroke-red-500 mr-2" src={ExclamationCircle} />
+					Изтекъл срок
+				</Typography>
+			{:else if item.node?.notifications?.find((e) => e?.type === 'hasEntriesThatWillExpire')}
+				<Typography class="flex">
+					<Icon class="w-[24px] h-[24px] stroke-yellow-500 mr-2" src={Exclamation} />
+					Изтичащ срок
+				</Typography>
+			{/if}
+			{#if item.node?.notifications?.find((e) => e?.type === 'expiredForNextExpedition')}
+				<Typography class="flex">
+					<Icon class="w-[24px] h-[24px] stroke-orange-500 mr-2" src={ExclamationCircle} />
+					Негодно за следваща експедиция
+				</Typography>
+			{/if}
+			{#if item.node?.notifications?.find((e) => e?.type === 'lowQuantity')}
+				<Typography class="flex">
+					<Icon class="w-[24px] h-[24px] stroke-yellow-500 mr-2" src={Exclamation} />
+					Ниско количество
+				</Typography>
+			{/if}
+			</div>
 	</CardContent>
 </Card>
-
